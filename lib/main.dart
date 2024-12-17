69d488bda1c8231e8aaba8339284bbfcb8123c2e
bipul <syedbipulrahman2@gmail.com>
2024-12-17 11:24:27 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,13 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
   late GoogleMapController _mapController;
   Location _location = Location();
   LatLng _currentPosition = const LatLng(0, 0);
-  LatLng _targetLocation =
-      const LatLng(23.8103, 90.4125); // Example: Dhaka, Bangladesh
+  LatLng _targetLocation = const LatLng(23.8103, 90.4125);
 
   Marker _userMarker =
       const Marker(markerId: MarkerId("user_marker"), position: LatLng(0, 0));
   Marker _targetMarker = const Marker(
     markerId: MarkerId("target_marker"),
-    position: LatLng(23.8103, 90.4125),
+    position: LatLng(23.748795207974442, 90.57620608223354),
     infoWindow: InfoWindow(title: "Target Location"),
   );
 
@@ -38,6 +37,48 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
   void initState() {
     super.initState();
     _initializeLocation();
+  }
+
+
+  void _fitMarkersInView() {
+    if (_currentPosition == const LatLng(0, 0)) return;
+
+    LatLngBounds bounds = LatLngBounds(
+      southwest: LatLng(
+        _currentPosition.latitude < _targetLocation.latitude
+            ? _currentPosition.latitude
+            : _targetLocation.latitude,
+        _currentPosition.longitude < _targetLocation.longitude
+            ? _currentPosition.longitude
+            : _targetLocation.longitude,
+      ),
+      northeast: LatLng(
+        _currentPosition.latitude > _targetLocation.latitude
+            ? _currentPosition.latitude
+            : _targetLocation.latitude,
+        _currentPosition.longitude > _targetLocation.longitude
+            ? _currentPosition.longitude
+            : _targetLocation.longitude,
+      ),
+    );
+
+    _mapController.animateCamera(CameraUpdate.newLatLngBounds(bounds, 100));
+  }
+
+  void _updateTargetLocation(LatLng newTarget) {
+    setState(() {
+      _targetLocation = newTarget;
+
+      _targetMarker = Marker(
+        markerId: const MarkerId("target_marker"),
+        position: newTarget,
+        infoWindow: const InfoWindow(title: "New Target Location"),
+      );
+
+      // Recalculate the route and fit the view
+      _fetchRoute();
+      _fitMarkersInView();
+    });
   }
 
   void _initializeLocation() async {
@@ -54,8 +95,10 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
 
   void _updateLocation(LocationData locationData) {
     setState(() {
-      _currentPosition =
-          LatLng(locationData.latitude!, locationData.longitude!);
+
+      _updateTargetLocation(_targetLocation);
+
+      _currentPosition = LatLng(locationData.latitude!, locationData.longitude!);
 
       _userMarker = Marker(
         markerId: const MarkerId("user_marker"),
@@ -79,15 +122,20 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
 
       // Fetch route and draw it
       _fetchRoute();
+
+      // Adjust zoom to fit both markers
+      _fitMarkersInView();
     });
   }
 
-  void _fetchRoute() async {
 
-    const String googleAPIKey = '_MAPS_API_KEY_';
+  void _fetchRoute() async {
+    const String googleAPIKey = 'api_key';
     Dio dio = Dio();
+
     String url =
         "https://maps.googleapis.com/maps/api/directions/json?origin=${_currentPosition.latitude},${_currentPosition.longitude}&destination=${_targetLocation.latitude},${_targetLocation.longitude}&key=$googleAPIKey";
+
     try {
       var response = await dio.get(url);
       if (response.statusCode == 200) {
@@ -109,6 +157,33 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
     }
   }
 
+
+  // void _fetchRoute() async {
+  //   const String googleAPIKey = 'AIzaSyCZ6YIiEkZnGVCQUyFIKsu3RdOJ49GVeLU';
+  //   Dio dio = Dio();
+  //   String url =
+  //       "https://maps.googleapis.com/maps/api/directions/json?origin=${_currentPosition.latitude},${_currentPosition.longitude}&destination=${_targetLocation.latitude},${_targetLocation.longitude}&key=$googleAPIKey";
+  //   try {
+  //     var response = await dio.get(url);
+  //     if (response.statusCode == 200) {
+  //       List<LatLng> routePoints = _decodePolyline(
+  //           response.data['routes'][0]['overview_polyline']['points']);
+  //
+  //       setState(() {
+  //         _polylines.clear();
+  //         _polylines.add(Polyline(
+  //           polylineId: const PolylineId('route'),
+  //           points: routePoints,
+  //           color: Colors.blue,
+  //           width: 5,
+  //         ));
+  //       });
+  //     }
+  //   } catch (e) {
+  //     print("Error fetching route: $e");
+  //   }
+  // }
+
   List<LatLng> _decodePolyline(String encoded) {
     PolylinePoints polylinePoints = PolylinePoints();
     List<PointLatLng> decodedPoints = polylinePoints.decodePolyline(encoded);
@@ -120,6 +195,7 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Route with Distance'),
@@ -138,6 +214,7 @@ class _LiveLocationAppState extends State<LiveLocationApp> {
               polylines: _polylines,
               onMapCreated: (GoogleMapController controller) {
                 _mapController = controller;
+                _fitMarkersInView();
                 _mapController.animateCamera(CameraUpdate.newLatLngBounds(
                   LatLngBounds(
                     southwest: LatLng(
